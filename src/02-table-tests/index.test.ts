8e1ab16bac53be581bee8973029a025395282591
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-07-03 20:21:28 +0400

    feat: add table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,28 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
+
+interface TestingData {
+  a: number | unknown;
+  b: number;
+  action: Action | unknown;
+  expected: number | null;
+}
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 10, b: 8, action: Action.Subtract, expected: 2 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 21, b: 7, action: Action.Divide, expected: 3 },
+  { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+  { a: 3, b: 2, action: 'x', expected: null },
+  { a: 'I', b: 2, action: Action.Add, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'Expected result should be equal to expected in the next object %s',
+    ({ a, b, action, expected }: TestingData) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
 });
